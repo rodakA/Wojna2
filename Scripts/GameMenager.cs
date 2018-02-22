5c189a0910dc2e82686219fa0097cd4bab7e4bec
Adam <L412@T510>
2018-02-22 21:28:27 +0100

    Pierwszy poprawny od teraz startujemy

--- Scripts/GameMenager.cs
+++ Scripts/GameMenager.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GameMenager : MonoBehaviour {
 
-    // nowa zmiana
-
     private int[] talia = new int [56];
     private int[] deck1 = new int[56];
     private int[] deck2 = new int[56];
